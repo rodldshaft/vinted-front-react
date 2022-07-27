69b45a82490eb2e65a84d434d32160e2b8ca535d
Gérold <gerold380@hotmail.com>
2022-07-27 17:21:35 +0200

    modified:   package.json 	modified:   src/App.css 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	modified:   yarn.lock
    	src/assets/
    	src/components/
    	src/pages/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>Navigation</nav>
+      <Link className="home" to="/home">
+        Home
+      </Link>
+      <br />
+      <Link to="/offer">Aller a la page offres</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+      </Routes>
+    </Router>
   );
 }
 
